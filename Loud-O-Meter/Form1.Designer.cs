89a67e40738bcf297fc300e40cf3c2a66e36d7f0
Martin Schulz <MartinSchulz2000@gmail.com>
2015-11-01 20:58:36 +0100

    + Audio Gerät mit visualisierung

--- Loud-O-Meter/Form1.Designer.cs
+++ Loud-O-Meter/Form1.Designer.cs
@@ -29,11 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pb_volume = new System.Windows.Forms.ProgressBar();
+            this.t_volume = new System.Windows.Forms.Timer(this.components);
+            this.l_volume = new System.Windows.Forms.Label();
+            this.nud_timer = new System.Windows.Forms.NumericUpDown();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_timer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_volume
+            // 
+            this.pb_volume.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_volume.Location = new System.Drawing.Point(12, 226);
+            this.pb_volume.Name = "pb_volume";
+            this.pb_volume.Size = new System.Drawing.Size(260, 23);
+            this.pb_volume.Step = 1;
+            this.pb_volume.TabIndex = 0;
+            // 
+            // t_volume
+            // 
+            this.t_volume.Enabled = true;
+            this.t_volume.Interval = 1;
+            this.t_volume.Tick += new System.EventHandler(this.t_volume_Tick);
+            // 
+            // l_volume
+            // 
+            this.l_volume.AutoSize = true;
+            this.l_volume.Location = new System.Drawing.Point(12, 210);
+            this.l_volume.Name = "l_volume";
+            this.l_volume.Size = new System.Drawing.Size(21, 13);
+            this.l_volume.TabIndex = 1;
+            this.l_volume.Text = "0%";
+            // 
+            // nud_timer
+            // 
+            this.nud_timer.Location = new System.Drawing.Point(222, 200);
+            this.nud_timer.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nud_timer.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_timer.Name = "nud_timer";
+            this.nud_timer.Size = new System.Drawing.Size(50, 20);
+            this.nud_timer.TabIndex = 2;
+            this.toolTip.SetToolTip(this.nud_timer, "Zeitangabe in Millisekunden");
+            this.nud_timer.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_timer.ValueChanged += new System.EventHandler(this.nud_timer_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.nud_timer);
+            this.Controls.Add(this.l_volume);
+            this.Controls.Add(this.pb_volume);
+            this.Name = "Form1";
+            this.Text = "Loud-O-Meter";
+            ((System.ComponentModel.ISupportInitialize)(this.nud_timer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar pb_volume;
+        private System.Windows.Forms.Timer t_volume;
+        private System.Windows.Forms.Label l_volume;
+        private System.Windows.Forms.NumericUpDown nud_timer;
+        private System.Windows.Forms.ToolTip toolTip;
     }
 }
 
--- Loud-O-Meter/Form1.cs
+++ Loud-O-Meter/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio;
+using NAudio.CoreAudioApi;
 
 namespace Loud_O_Meter
 {
@@ -15,6 +17,21 @@ namespace Loud_O_Meter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void t_volume_Tick(object sender, EventArgs e)
+        {
+            MMDeviceEnumerator device = new MMDeviceEnumerator();
+            MMDevice mic = device.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            float vol = mic.AudioMeterInformation.MasterPeakValue*100;
+            pb_volume.Value = (int)vol;
+
+            l_volume.Text = (int)vol + "%";
+        }
+
+        private void nud_timer_ValueChanged(object sender, EventArgs e)
+        {
+            t_volume.Interval = (int)nud_timer.Value;
         }
     }
 }
